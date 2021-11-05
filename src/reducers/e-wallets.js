567ab71befe41509c7317d8cca9b23aa5a5d179d
Eki Bagas <ekibagas99@gmail.com>
2021-11-05 13:22:54 +0700

    change api neh

--- src/reducers/e-wallets.js
+++ src/reducers/e-wallets.js
@@ -1,14 +1,14 @@
 import { EWALLET_CHARGE, GET_STATUS } from "../constants/actionTypes";
 
 const initialState = {
-  isLoading: true,
   ewallets: [],
 };
 
 const ewallets = (state = initialState, action) => {
   switch (action.type) {
     case EWALLET_CHARGE:
-      return { ...state, ewallets: action.payload.data };
+      return { ...state, ewallets: [...state.ewallets, action?.payload] };
+    // return { ...state, ewallets: action.payload.data };
     case GET_STATUS:
       return { ...state, ewallets: action.payload.data };
     default:
