335303e61be25d0cd902e18af8e250aecea6a152
Eki Bagas <ekibagas99@gmail.com>
2021-11-07 14:30:38 +0700

    change apis

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://1ca6-115-178-227-142.ngrok.io" });
+const API = axios.create({ baseURL: "https://topupgamesku.herokuapp.com" });
 
 export const fetchAllSlides = () => API.get(`/api/slides`);
 
